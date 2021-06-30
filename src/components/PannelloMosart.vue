ed9548cd1aa92cf2190c666ef4c1b41c79e9f3e9
niksolaz <solazzo.nicola@gmail.com>
2021-06-30 12:34:51 +0200

    Aggiunto template nuovi

---
+++ src/components/PannelloMosart.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        Pannello Mosart
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Principale.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="team">
+        <Destinazioni />
+        <PannelloMosart />
+        <DebugPannel />
+    </div>
+</template>
+
+<script>
+import Destinazioni from './Destinazioni'
+import PannelloMosart from './PannelloMosart'
+import DebugPannel from './DebugPannel'
+export default {
+  name: 'team',
+  components: {
+    Destinazioni,
+    PannelloMosart,
+    DebugPannel
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
